8be106e5bae1b1ab3774cee06c9aa30412504602
Cam Spiers <cameron@heyday.co.nz>
2012-12-06 14:22:58 +1300

    UPDATE: Semi working for basic traits

--- src/Camspiers/TraitConvert/Converter.php
+++ src/Camspiers/TraitConvert/Converter.php
@@ -15,12 +15,6 @@ class Converter
 		} else {
 			throw new InvalidArgumentException('$classes must be an array of classes');			
 		}
-
-		if (is_writable($ouputDirectory)) {
-			$this->outputDirectory = $outputDirectory;
-		} else {
-			throw new InvalidArgumentException('$outputDirectory must be a writable directory');		
-		}
 	}
 
 	public function convert()
@@ -28,21 +22,34 @@ class Converter
 
 		foreach ($this->classes as $class) {
 			
-			$obj = new ReflectionClass($class);
+			$obj = new \ReflectionClass($class);
 			$traits = $obj->getTraits();
 			$content = '';
 
 			if (is_array($traits)) {
 				foreach ($traits as $trait) {
-					$trait = new ReflectionClass($trait);
-					$content .= implode('', array_slice(file($trait->getFileName()), $trait->getStartLine(), $trait->getEndLine()));
+					$content .= implode(
+						PHP_EOL,
+						array_slice(
+							file($trait->getFileName(), FILE_IGNORE_NEW_LINES),
+							$trait->getStartLine() + 1,
+							$trait->getEndLine() - $trait->getStartLine() - 2
+						)
+					);
 				}
 
 			}
 
-			$file = file($obj->getFileName());
+			$file = file($obj->getFileName(), FILE_IGNORE_NEW_LINES);
 
-			array_splice($file, $obj->getEndLine(), 0, $content);
+			array_splice($file, $obj->getEndLine() - 1, 0, $content);
+
+			// foreach ($file as $key => $value) {
+			// 	if (is_array($traits)) {
+			// 		foreach ($traits as $trait) {
+			// 		}
+			// 	}
+			// }
 
 			file_put_contents($obj->getFileName(), implode(PHP_EOL, $file));
 
